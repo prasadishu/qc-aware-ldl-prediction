82e134c2d89e3588b0a11734d52dcf710e8bb99b
prasadishu <vdvprasad.24@gmail.com>
2026-02-25 13:17:19 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+def load_dataset(path):
+    df = pd.read_excel(path)
+    required_cols = ["TC", "TG", "HDL_C", "LDL_direct"]
+    df = df.dropna(subset=required_cols)
+    return df
