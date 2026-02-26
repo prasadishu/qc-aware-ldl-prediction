a2ec2f2099139312eaaab692d3aab0e5d6539b4e
prasadishu <vdvprasad.24@gmail.com>
2026-02-27 01:15:49 +0530

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,3 +1,6 @@
+from sklearn.metrics import r2_score, mean_squared_error
+import numpy as np
+
 def evaluate(y_true, y_pred):
 
     r2 = r2_score(y_true, y_pred)
@@ -6,9 +9,3 @@ def evaluate(y_true, y_pred):
     pcc = np.corrcoef(y_true, y_pred)[0, 1]
 
     return r2, rmse, mse, pcc
-  r2, rmse, mse, pcc = evaluate(y_test, predictions)
-
-print(f"R²: {r2:.3f}")
-print(f"RMSE: {rmse:.3f}")
-print(f"MSE: {mse:.3f}")
-print(f"PCC: {pcc:.3f}")
