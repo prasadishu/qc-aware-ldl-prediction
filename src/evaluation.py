9a0524ca9e910f101d76a0f320b2d181554a9a6e
prasadishu <vdvprasad.24@gmail.com>
2026-02-27 21:48:10 +0530

    Fix variable name for r2 in evaluation function

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
 def evaluate(y_true, y_pred):
 return {
-r2 = r2_score(y_true, y_pred)
+r^2 = r^2_score(y_true, y_pred)
 mse = mean_squared_error(y_true, y_pred)
 rmse = np.sqrt(mse)
 pcc = np.corrcoef(y_true, y_pred)[0, 1]
