456f9875f05b55b3c2a082a14d495ce47068a6c7
prasadishu <vdvprasad.24@gmail.com>
2026-02-27 13:21:57 +0530

    Update load_dataset to drop all NaN values
    Remove specific column filtering for NaN values in dataset loading.

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,6 +2,5 @@ import pandas as pd
 
 def load_dataset(path):
     df = pd.read_excel(path)
-    required_cols = ["TC", "TG", "HDL_C", "LDL_direct"]
-    df = df.dropna(subset=required_cols)
+    df = df.dropna()
     return df
