0e02ece60694f6e0abd12b7b29a96c84e4b673b4
prasadishu <vdvprasad.24@gmail.com>
2026-02-27 13:18:21 +0530

    Refactor get_models function for model initialization
    Refactored the get_models function to simplify model initialization by removing specific parameters and the pipeline for SVR.

--- src/models.py
+++ src/models.py
@@ -1,32 +1,13 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVR
-from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 
 def get_models():
-
-    return {
-        "CatBoost": CatBoostRegressor(
-            iterations=300,
-            depth=6,
-            learning_rate=0.05,
-            verbose=0
-        ),
-        "XGBoost": XGBRegressor(
-            n_estimators=300,
-            max_depth=6,
-            learning_rate=0.05,
-            objective="reg:squarederror",
-            random_state=42
-        ),
-        "RandomForest": RandomForestRegressor(
-            n_estimators=300,
-            random_state=42
-        ),
-        "SVR": Pipeline([
-            ("scaler", StandardScaler()),
-            ("svr", SVR(kernel="rbf", C=50, epsilon=0.1))
-        ])
+    models = {
+        "CatBoost": CatBoostRegressor(verbose=0),
+        "XGBoost": XGBRegressor(),
+        "RandomForest": RandomForestRegressor(),
+        "SVR": SVR(kernel="rbf")
     }
+    return models
