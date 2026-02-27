c3a0d3b6fd310ff3bcd8befd24c2b03576ababba
prasadishu <vdvprasad.24@gmail.com>
2026-02-27 21:45:13 +0530

    Fix syntax for returning evaluation metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,11 +1,10 @@
 import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
-
 def evaluate(y_true, y_pred):
-    return {
-    r2 = r2_score(y_true, y_pred)
-    mse = mean_squared_error(y_true, y_pred)
-    rmse = np.sqrt(mse)
-    pcc = np.corrcoef(y_true, y_pred)[0, 1]
-    }
+return {
+r2 = r2_score(y_true, y_pred)
+mse = mean_squared_error(y_true, y_pred)
+rmse = np.sqrt(mse)
+pcc = np.corrcoef(y_true, y_pred)[0, 1]
+}
    
