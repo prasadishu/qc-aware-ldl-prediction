ec84eb47267cd24ea861be4dc24a4b241ecfcfe9
prasadishu <vdvprasad.24@gmail.com>
2026-02-28 11:01:24 +0530

    Create evaluation.py

---
+++ src/evaluation.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error
+
+def evaluate(y_true, y_pred):
+    r2 = r2_score(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    pcc = np.corrcoef(y_true, y_pred)[0, 1]
+    return r2, rmse, mse, pcc
